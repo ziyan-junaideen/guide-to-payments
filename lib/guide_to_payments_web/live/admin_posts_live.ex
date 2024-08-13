0d6c94dea7c9f0105a3698f42be0c39cec07db77
Ziyan Junaideen <ziyan@tryedge.com>
2024-08-13 18:32:33 +0530

    Move admin posts view to its own file

--- lib/guide_to_payments_web/live/admin_posts_live.ex
+++ lib/guide_to_payments_web/live/admin_posts_live.ex
@@ -3,34 +3,6 @@ defmodule CoreWeb.AdminPostsLive do
   import Ecto.Query
 
   @impl Phoenix.LiveView
-  def render(assigns) do
-    ~H"""
-    <h1>Welcome to the Admin Blog Posts</h1>
-    <table>
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Views</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <%= for post <- @posts do %>
-          <tr>
-            <td><%= post.title %></td>
-            <td><%= post.views %></td>
-            <td>
-              <.link href={~p"/admin/posts/#{post.id}/edit"}>Edit</.link>
-              <.link href={~p"/posts/#{post.slug}"}>View</.link>
-            </td>
-          </tr>
-        <% end %>
-      </tbody>
-    </table>
-    """
-  end
-
-  @impl Phoenix.LiveView
   def handle_params(params, _uri, socket) do
     page = params["page"] || 1
     per_page = 10
